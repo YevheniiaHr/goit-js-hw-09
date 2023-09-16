165922ae3402d5e824adc81b82e788e0884587b9
YevheniiaHr <131554616+YevheniiaHr@users.noreply.github.com>
2023-09-16 21:32:22 +0200

    format

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,31 +1,25 @@
-const startBtn = document.querySelector("[data-start]");
-const stopBtn = document.querySelector("[data-stop]");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
-let timeId
+let timeId;
 
-
- 
 startBtn.addEventListener('click', () => {
- timeId = setInterval(() => {
- const randomColor =  getRandomHexColor();
- document.body.style.backgroundColor = randomColor;
-}, 1000)
-startBtn.disabled = true;
-stopBtn.disabled = false;
+  timeId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
 });
 
 stopBtn.addEventListener('click', () => {
- clearInterval(timeId);
- startBtn.disabled = false;
-stopBtn.disabled = true;
-})   
-      
-        
-function getRandomHexColor() {
- return `#${Math.floor(Math.random() * 16777215)
- .toString(16)
- .padStart(6, 0)}`;
-};
-  
+  clearInterval(timeId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
 
-  
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,32 +1,29 @@
 import Notiflix from 'notiflix';
 
-
-
-
-
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', onSubmitForm);
-
 
 function onSubmitForm(event) {
   event.preventDefault();
 
   let delay = Number(form.delay.value);
- 
 
   for (let i = 1; i <= form.amount.value; i += 1) {
     createPromise(i, delay)
       .then(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
       })
       .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
       });
     delay += Number(form.step.value);
   }
-
-};
+}
 
 function createPromise(position, delay) {
   const obj = { position, delay };
@@ -35,7 +32,7 @@ function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-       resolve(obj);
+        resolve(obj);
       } else {
         reject(obj);
       }
